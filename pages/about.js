e9f284c8f736b912259208fc0bf49871c9492c39
SupremeSeymour <robyteoneoneone@gmail.com>
2022-09-14 12:25:27 -0700

    fixed

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+        <div>
+            <h1>This page explains what we about up here!</h1>
+            <Link href="/"><a> Home </a></Link>
+        </div>
+    );
+}
