cc7c8a068952a9617a6b6aa44809695db8de2f29
SupremeSeymour <robyteoneoneone@gmail.com>
2022-09-14 12:36:34 -0700

    another commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
 module.exports = {
   exportTrailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/'}
+    };
+  }
 };
